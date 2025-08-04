493a8285649f128b2a50a4461c07c0c17ac0e365
Deepak432-ai <deepak_2312res239@iitp.ac.in>
2025-08-05 02:22:03 +0530

    index.js changes

--- index.js
+++ index.js
@@ -7,14 +7,24 @@ const voteRoutes = require('./routes/voteRoutes');
 
 
 const app = express();
+
 const corsOptions = {
-  origin: process.env.NODE_ENV === 'production'
-    ? process.env.CLIENT_ORIGIN 
-    : 'http://localhost:5173',
+  origin: (origin, callback) => {
+    const allowedOrigins = [
+      'http://localhost:5173',
+      process.env.CLIENT_ORIGIN,
+    ];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-user-email'],
 };
+
 
 app.use(cors(corsOptions));
 
